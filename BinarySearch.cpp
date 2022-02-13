2dac7256f276d267bb8119859b7fa6c66bebbb0c
Phoebe8 <50315023+Phoebe8@users.noreply.github.com>
2022-02-13 21:27:29 +0800

    Add files via upload

---
+++ BinarySearch.cpp
@@ -0,0 +1,41 @@
+// ¶þ·Ö²éÕÒ
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right)
+        {
+            int middle = left + ((right - left) >> 1);
+            if (nums[middle] > target)
+            {
+                right = middle - 1;
+            }
+            else if (nums[middle] < target)
+            {
+                left = middle + 1;
+            }
+            else
+            {
+                return middle;
+            }
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    int a[] = { -1,0,3,5,9,12 };
+    vector<int> nums(a, a + sizeof(a) / sizeof(int));
+    Solution solution;
+    cout << solution.search(nums, 9) << endl;
+
+    system("pause");
+    return 0;
+}
